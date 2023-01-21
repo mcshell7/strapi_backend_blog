6b3603db0892aaab49e081a346055ca821c13a29
Mykhailo Khamardiuk <mishakhamardiuk@gmail.com>
2023-01-20 22:04:10 -0500

    updated packages file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,10 +5,11 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 7284),
+      port: env.int('PGPORT', 1337),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'password'),
     },
   },
 });
+
